5106dbbce66ec17dbccf3bb6b124a2a255a17989
agiknightt <70384293+agiknightt@users.noreply.github.com>
2020-08-31 16:12:39 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int granny;
+            int totalReceptionTime;
+            int hour;
+            int minute;
+            int timeReceipt = 10;
+
+            Console.Write("Сколько людей в очереди? : ");
+            granny = Convert.ToInt32(Console.ReadLine());
+
+            totalReceptionTime = granny * timeReceipt;
+            hour = totalReceptionTime / 60;
+            minute = totalReceptionTime % 60;
+            Console.WriteLine($"Вы будете стоять в очереди {hour} часа и {minute} минут");
+            Console.ReadKey();
+        }
+    }
+}
